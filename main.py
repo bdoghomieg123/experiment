08cb11b3ba6b2b667fc83caa0338a1f697607267
Ben <capnben2009@hotmail.com>
2020-11-25 08:32:48 -0500

    P1

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import praw
 import time
-import docx
-
+import os
+from docx import *
+from common import *
 
 
 reddit = praw.Reddit('bot1')
-print(f"{reddit}")
 
-#test
+subreddit = reddit.subreddit("politics")
+
+if not os.path.isfile("posts.docx"):
+    document = Document()
+    document.save("posts.docx")
+    print("Created Docx file within working directory")
+    time.sleep(1)
+    clear()
+
+elif os.path.isfile("posts.docx"):
+    print("File exists. Continuing")
+    time.sleep(1)
+    clear()
+
+
+while True:
+    for submission in subreddit.controversial():
+        sub = submission.subreddit
+        if sub.over18:
+            print("Subreddit is NSFW")
+        elif sub == "politics":
+            print("Subreddit is Politics")
+
+
+        else:
+            print(submission.title)"""
